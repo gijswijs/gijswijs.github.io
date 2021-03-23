3a26223b8581e71c5ec8564fdd1d153c0558b92e
Gijs van Dam <gvandam@gmail.com>
2021-03-23 17:50:12 +0800

    Wasm test

---
+++ nuxt/df37d4c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1179:function(n,e,l){"use strict";l.r(e);var t=l(36),component=Object(t.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("Hallo")])}),[],!1,null,null,null);e.default=component.exports}}]);
