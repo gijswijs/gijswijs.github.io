0694be6c711fcb58bc1c42344d13bd63c8b97912
Gijs van Dam <gvandam@gmail.com>
2021-10-08 15:16:17 +0800

    master run

--- nuxt/5c6ba35.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1195:function(n,e,l){"use strict";l.r(e);var t=l(36),component=Object(t.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("Hallo")])}),[],!1,null,null,null);e.default=component.exports}}]);
