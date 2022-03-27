0324cd70936407f18a917f32f95b625f92d1d343
Gijs van Dam <gvandam@gmail.com>
2022-03-27 11:33:09 +0800

    Upgrade to Janos v0.5.0

--- nuxt/e655111.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{1191:function(n,e,t){"use strict";t.r(e);var l={layout:"centered",auth:!1},c=t(36),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("Logging in...")])}),[],!1,null,null,null);e.default=component.exports}}]);
